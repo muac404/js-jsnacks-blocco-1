78b1208d3296c1540011f2e71f5135cf0d20a25d
muac404 <muac404@gmail.com>
2022-10-26 11:44:26 +0200

    Snack2

---
+++ jsnack2/main.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const number1 = Number(prompt("Inserisci un numero"));
+const number2 = Number(prompt("Inserisci un numero"));
+const number3 = Number(prompt("Inserisci un numero"));
+const number4 = Number(prompt("Inserisci un numero"));
+const number5 = Number(prompt("Inserisci un numero"));
+const number6 = Number(prompt("Inserisci un numero"));
+const number7 = Number(prompt("Inserisci un numero"));
+const number8 = Number(prompt("Inserisci un numero"));
+const number9 = Number(prompt("Inserisci un numero"));
+const number10 = Number(prompt("Inserisci un numero"));
+
+console.log(number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10)
+
+
+
+
+// for (let i = 0; i < 10; index++) {
+//     const number1 = Number(prompt("Inserisci un numero"));
+    
+// }
