10aecda0a3991a8315c30e4c952ff48d37962616
muac404 <muac404@gmail.com>
2022-10-26 10:58:23 +0200

    first commit

---
+++ jsnack1/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const word1 = prompt("Inserisci una parola");
+const word2 = prompt ("inserisci un'altra parola");
+
+
+
+if (word1.lenght > word2.lenght){
+    console.log(word1, word2)
+} else {
+    console.log (word2, word1)
+}
