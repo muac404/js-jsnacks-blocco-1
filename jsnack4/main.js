5ba2f14cba443a64c472387515930736536c6505
muac404 <muac404@gmail.com>
2022-10-26 12:42:23 +0200

    Aggiune console log numero dispari

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -8,6 +8,8 @@ for (let i = 0; i < 6; i++) {
     const number = Number(prompt("Inserisci un numero"));
     if (number % 2 === 0){
         arrayE.push(number)
+    } else{
+       console.log(number, "Numero dispari")
     }
 
     console.log(arrayE)
